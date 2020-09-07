a8d1bc38b76b6aa83e859314d719cba5632d1226
RiccardoSbalchiero <70885294+RiccardoSbalchiero@users.noreply.github.com>
2020-09-07 10:30:16 +0200

    Update game.java

--- game.java
+++ game.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 class game
 {
+  //creating simple ASCII images of a pet with no JFrame and make it like a pet simulator ^_^.
   public static void main (String[] args)
   {
     System.out.println("welcome to this project");
